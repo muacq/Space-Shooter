fa3b37809ced5ce2adc8d3e507db19719d2fbf47
muacq <237768202@qq.com>
2018-10-16 20:06:05 +0800

    test

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValue;
+    public int hazardCount;
+
+    public float startDelay;
+    public float hazardDelay;
+    public float waveDelay;
+
+    private int score;
+    public Text scoreText;
+
+    public Text gameOverText;
+    private bool gameOver;
+
+    public Text restartText;
+    private bool restart;
+
+    // Use this for initialization
+    void Start () {
+        gameOverText.text = "";
+        gameOver = false;
+        restartText.text = "";
+        restart = false;
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWave());
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                //Application.LoadLevel(Application.loadedLevel);  deprecated
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        gameOverText.text = "Game Over";
+    }
+	
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void addScore(int value)
+    {
+        score += value;
+        UpdateScore();
+    }
+
+	IEnumerator SpawnWave () {
+        yield return new WaitForSeconds(startDelay);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x),
+                    spawnValue.y,
+                    spawnValue.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(hazardDelay);
+
+                if (gameOver)
+                {
+                    restart = true;
+                    restartText.text = "Press 'R' to Restart";
+                }
+            }
+            yield return new WaitForSeconds(waveDelay);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController1 : MonoBehaviour {
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    private float nextFire;
+    public float fireRate;
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        GetComponent<Rigidbody>().position = new Vector3(
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            );
+    }
+
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
